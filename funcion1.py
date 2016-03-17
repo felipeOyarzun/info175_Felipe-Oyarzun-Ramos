3df42ddb49077377f1d5a1f77726f792c83e7798
felipe <mimox3d@gmail.com>
2016-03-17 12:24:37 -0300

    ejercicio 1

---
+++ funcion1.py
@@ -0,0 +1,10 @@
+print ("iniciando programa")
+def comparar():
+    x =input("ingrese un numero")
+    y = input("ingrese otro numero")
+    print ("el mayor es ")
+    if (x<y):
+        return y
+    if (y>x):
+        return x
+    
