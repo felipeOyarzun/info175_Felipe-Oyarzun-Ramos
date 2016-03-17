f305cbdbb1e3c66eeec85dbcf623bcd9f02dfe20
felipe <mimox3d@gmail.com>
2016-03-17 13:00:23 -0300

    subiendo primera resolucion del ejercicio 2

---
+++ funcion2.py
@@ -0,0 +1,16 @@
+
+
+def funcion2():
+    xs=[]
+    palabras = "no vacio"
+    while(palabras != ""):
+        palabras = input("ingrese palabras")
+        xs.append(palabras)
+        print "fin"
+    print "listo"
+    xs.sort()
+    print xs
+    
+     
+     
+     
